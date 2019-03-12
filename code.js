9f7b5e9a99f459e17ebf28811f6706612b0ef76b
Natalya Dolgireva <suhoikova@gmail.com>
2019-03-12 20:55:12 +0300

    WIP

--- code.js
+++ code.js
@@ -5,33 +5,34 @@ var album = document.querySelector('.album');
 var galleryItem = document.querySelector('.galleryItem').content;
 var imgSmall = document.getElementById('imgThumb_imgid');
 
+var imgAll = [
+  "img/background-1.JPG", 
+  "img/background-2.JPG",
+  "img/background-3.JPG",
+  "img/background-4.JPG",
+  "img/background-5.JPG",
+  "img/background-6.JPG",
+  "img/background-7.JPG",
+  "img/background-8.JPG"];
+
 //создаем галарею  - картинки снизу  */
 var renderItems = function () {
   let newItem; // объявим новый элемент
   let roulette = document.createDocumentFragment (); 
   /* // создаем рулетку зависимость галереи от  количества изображений. Добавление элементов в рулетку */
 
-  for (let i = 0; i < 8; i++) {
+  for (let i = 0; i < imgAll.length; i++) {
       
       newItem = galleryItem.cloneNode(true);
       //newItem.querySelector('.imgThumb').classList.add('gThumb');
       newItem.querySelector('.imgThumb').classList.add('thumb' + i);
-      newItem.querySelector('.imgThumb__img').src = "img/background-" + (i + 1) + ".JPG";
+      newItem.querySelector('.imgThumb__img').src = imgAll[i];
       roulette.appendChild(newItem);
   }
   album.appendChild(roulette);
 };
 renderItems();
 
-//Функция, которая определяет цифру в названии файла картинки и возвращает ее значение
-// (ограничение макс 9 файлов)
-var findNumbInNameImg = function() {
-  let str = imgBig.src; //cтрока содержащаяся в src картинки Большой
-  
-  return parseInt(str[(str.length - 5)]);
-  //все файлы называются однотипно и 5й символ с конца это номер файла
-  //минусы этого решения, сломается если картинок больше 9, но можно добавить условия и все будет работать
-};
 
 //Функция смены главной картинки при клике кнопки next
 var changeSlidesNext = function () {
@@ -44,16 +45,6 @@ var changeSlidesNext = function () {
   }
 };
 
-//Функция смены главной картинки при клике кнопки Prev
-var changeSlidesPrev = function () {
-  let counter = findNumbInNameImg();
-  if ((counter - 1) > 0) {
-    imgBig.src = "img/background-" + (counter - 1) + ".JPG";
-  } else {
-    counter = 8;
-    imgBig.src = "img/background-" + (counter - 1) + ".JPG";
-  }
-};
 
 //Функция увеличения картинки при клике на маленькую снизу
 //через target определяем у картинки путь evt.target.src и присваиваем к большой 
