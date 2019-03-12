6434a80588d2de09edc7903be9d3e76d1fd19918
Natalya Dolgireva <suhoikova@gmail.com>
2019-03-13 00:16:07 +0300

    Improve the functions

--- code.js
+++ code.js
@@ -5,15 +5,17 @@ var album = document.querySelector('.album');
 var galleryItem = document.querySelector('.galleryItem').content;
 var imgSmall = document.getElementById('imgThumb_imgid');
 
+var currentImageId = 0;// dataset.id   маленькой активной картинки
+
 var imgAll = [
-  "img/background-1.JPG", 
+  "img/background-0.JPG", 
+  "img/background-1.JPG",
   "img/background-2.JPG",
   "img/background-3.JPG",
   "img/background-4.JPG",
   "img/background-5.JPG",
   "img/background-6.JPG",
-  "img/background-7.JPG",
-  "img/background-8.JPG"];
+  "img/background-7.JPG"];
 
 //создаем галарею  - картинки снизу  */
 var renderItems = function () {
@@ -25,6 +27,7 @@ var renderItems = function () {
       newItem = galleryItem.cloneNode(true);
       //newItem.querySelector('.imgThumb').classList.add('gThumb');
       newItem.querySelector('.imgThumb').classList.add('thumb' + i);
+      newItem.querySelector('.imgThumb__img').dataset.id = i;
       newItem.querySelector('.imgThumb__img').src = imgAll[i];
       roulette.appendChild(newItem);
   }
@@ -32,43 +35,47 @@ var renderItems = function () {
 };
 renderItems();
 
-//Функция, которая определяет цифру в названии файла картинки и возвращает ее значение
-// (ограничение макс 9 файлов)
-var findNumbInNameImg = function() {
-  let str = imgBig.src; //cтрока содержащаяся в src картинки Большой
-  return parseInt(str[(str.length - 5)]);
-  //все файлы называются однотипно и 5й символ с конца это номер файла
-  //минусы этого решения, сломается если картинок больше 9, но можно добавить условия и все будет работать
-};
-
-
-//Функция смены главной картинки при клике кнопки next
+//Функция смены главной картинки при клике кнопки Next
 var changeSlidesNext = function () {
-  let counter = findNumbInNameImg();
-  if ((counter + 1) < 9) {
-    imgBig.src = "img/background-" + (counter + 1) + ".JPG";
+  newId = currentImageId;
+  if (newId < 7) {
+    changeMainPicture(newId + 1);
   } else {
-    counter = 1;
-    imgBig.src = "img/background-" + (counter + 1) + ".JPG";
+    changeMainPicture(0);
   }
 };
 
 //Функция смены главной картинки при клике кнопки Prev
 var changeSlidesPrev = function () {
-  let counter = findNumbInNameImg();
-  if ((counter - 1) > 0) {
-    imgBig.src = "img/background-" + (counter - 1) + ".JPG";
+  newId = currentImageId;
+  if (newId > 0) {
+    changeMainPicture(newId - 1);
   } else {
-    counter = 8;
-    imgBig.src = "img/background-" + (counter - 1) + ".JPG";
+    changeMainPicture(7);
   }
 };
+//Смена большой картинки при нажатии на маленькую. 
+var galleryApp = function (evt) {
+  id = evt.target.dataset.id; //определяем id у маленькой картинки
+  changeMainPicture(id);//  используем полученный ID для смены большой картинки
+};
+//
+var changeMainPicture = function(id) {
+  imgBig.src = getImageSrc(id);
+  console.log(currentImageId);
+  return currentImageId = id; 
+};
+
+var getImageSrc = function(id) {
+  return 'img/background-' + id +'.JPG'
+};
+
 //Функция увеличения картинки при клике на маленькую снизу
 //через target определяем у картинки путь evt.target.src и присваиваем к большой 
 var galleryApp = function (evt) {
-    imgBig.src = evt.target.src; 
+  imgBig.src = evt.target.src; 
 };
 
-window.addEventListener ('click', function (evt) {console.log(evt);})
+//window.addEventListener ('click', function (evt) {console.log(evt);})
 album.addEventListener ('click', galleryApp);
 imgBig.addEventListener ('click', changeSlidesNext);
